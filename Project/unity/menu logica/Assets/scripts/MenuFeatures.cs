3b22637b874b2c64e4912616db2709c581f8a763
Albert Folch <albertfolch58@hotmail.com>
2014-10-06 12:05:20 +0200

    Logica menu, primer commit

---
+++ Project/unity/menu logica/Assets/scripts/MenuFeatures.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuFeatures : MonoBehaviour
+{
+
+		public Texture tex1, tex2, tex3;
+		public GameObject medio;
+		private int clicks;
+
+		void Start ()
+		{
+				
+				medio = GameObject.FindGameObjectWithTag ("medio");
+				medio.renderer.material.mainTexture = tex1;
+				clicks = 1;
+
+		}
+	
+		void OnMouseDown ()
+		{
+				
+				
+
+				clicks += 1;
+						
+				if (clicks == 1) {
+						medio.renderer.material.mainTexture = tex1;
+				
+				} else if (clicks == 2) {
+						
+						//Texture2D tex = (Texture2D)Resources.Load ("imatges/destral", typeof(Texture2D));
+						medio.renderer.material.mainTexture = tex2;
+								
+				} else if (clicks == 3) {
+						medio.renderer.material.mainTexture = tex3;
+						clicks = 0;
+				}
+						
+				
+			   			
+
+
+		}
+}
---
+++ Project/unity/menu logica/Assets/scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
+
+public class Player : MonoBehaviour, InterfacePlayer
+{
+		public List <Feature> features;
+
+		void Awake ()
+		{
+				DontDestroyOnLoad (gameObject);
+
+		}
+		// Use this for initialization
+		void Start ()
+		{
+			features = new List<Feature> ();
+		}
+	
+		// Update is called once per frame
+		void Update ()
+		{
+	
+		}
+
+		public void addFeature (string caracteristica)
+		{
+				Feature f = new Feature (caracteristica);
+				this.features.Add (f);
+		}
+
+		public void movePlayer (float x, float y, float z)
+		{
+			Debug.Log ("Not implemented yet");
+
+		}
+
+		public List<Feature> getFeatures ()
+		{
+				return features;
+		}
+}
