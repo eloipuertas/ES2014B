a1575c0b755acfca68a6b9689a12880279221ba2
xavierfebrer <xavierfebrer@gmail.com>
2014-10-18 20:07:15 +0200

    added all the movement bugfixes

--- Project/unity/Assets/spider/scripts/NPCState.cs
+++ Project/unity/Assets/spider/scripts/NPCState.cs
@@ -3,63 +3,52 @@ using System.Collections;
 
 public class NPCState : MonoBehaviour {
 	
-	public float moveVelocity = 10f;
+	public float moveSpeed = 1f;
 	public int maxHealth = 100;
 	public int health = 100;
 	public Vector3 destination;
+	CharacterController characterController;
 	
 	void Awake(){
-		destination = transform.position;
-		//destination = new Vector3(100,100,100);
-		//animation = gameObject.animation;
-		//gameObject.animation.Play("walk",PlayMode.StopAll);
-		/*if(!animation.isPlaying){
-		}*/
+		characterController = GetComponent<CharacterController>();
+		setDestination(transform.position.x,transform.position.y,transform.position.z);
 	}
 	
 	void Update(){
-		moveToDestination();
+		move();
 	}
 	
-	public void moveToDestination(){
-		Vector3 direction = destination-transform.position;
-		if(direction.magnitude > 1){
-			direction.Normalize();
-			//rigidbody.velocity = direction*moveVelocity*Time.deltaTime;
-			rigidbody.velocity = direction - transform.position;
-		}else{
-			transform.position = destination;
-		}
+	private void move(){
+		Vector3 moveDirection = destination-transform.position;
+		moveDirection.Normalize();
+		moveDirection *= moveSpeed;
+		characterController.Move(moveDirection * Time.deltaTime);
 	}
 	
+	// MOVEMENT
 	public Vector3 getDestination(){
 		return destination;
 	}
-	
 	public void setDestination(float x,float y,float z){
 		destination = new Vector3(x,y,z);
 	}
 	
+	// HP
 	public void setHealth(int newHealth){
 		health = newHealth;
 	}
-	
 	public int getHealth(){
 		return health;
 	}
-	
 	public void setMaxHealth(int newMaxHealth){
 		maxHealth = newMaxHealth;
 	}
-	
 	public int getMaxHealth(){
 		return maxHealth;
 	}
-	
 	public void addHealth(int healthToAdd){
 		setHealth(health+healthToAdd);
 	}
-	
 	public void substractHealth(int healthToSubstract){
 		setHealth(health-healthToSubstract);
 	}
