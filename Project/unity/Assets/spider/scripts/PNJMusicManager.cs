5e1cfba05ea7d185c61878ff77a5c9d99da1e7b1
voriol <vkoriol@gmail.com>
2014-10-19 15:33:43 +0200

    PNJ music manager +sounds

---
+++ Project/unity/Assets/spider/scripts/PNJMusicManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ RequireComponent( typeof( AudioSource ) ) ]
+
+public class PNJMusicManager : MonoBehaviour 
+{
+	public AudioClip attackOK;
+
+	public AudioClip PNJKilled;
+
+	public void PlayAttackOK()
+	{
+		audio.clip = attackOK;
+		audio.Play ();
+	}
+	
+	public void PlayPNJKilled()
+	{
+		audio.clip = PNJKilled;
+		audio.Play ();		
+	}
+
+}
