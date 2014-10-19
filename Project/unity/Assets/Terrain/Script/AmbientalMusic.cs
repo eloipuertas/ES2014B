f93bdcfafba12aa3accb195406bb970fe318e5c2
voriol <vkoriol@gmail.com>
2014-10-19 19:06:27 +0200

    new script AmbientalMusic

---
+++ Project/unity/Assets/Terrain/Script/AmbientalMusic.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ RequireComponent( typeof( AudioSource ) ) ]
+
+public class AmbientalMusic : MonoBehaviour {
+
+	public AudioClip gotes;
+	public AudioClip ratPanat;
+	public AudioClip correntAire;
+	public AudioClip correntAigua;
+
+	public AudioClip fight;
+
+	bool PNJinScene = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ( PNJinScene ) {
+				audio.clip = fight;
+				audio.Play ();
+
+		} else {
+
+		}
+	
+	}
+}
