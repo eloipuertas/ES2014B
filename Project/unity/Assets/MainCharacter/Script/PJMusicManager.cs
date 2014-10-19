3ddde2c97afb3cd316efb31c95885e42c77d3124
voriol <vkoriol@gmail.com>
2014-10-19 14:57:14 +0200

    #50 audio +Music manager

---
+++ Project/unity/Assets/MainCharacter/Script/PJMusicManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ RequireComponent( typeof( AudioSource ) ) ]
+
+public class PJMusicManager : MonoBehaviour 
+{
+	public AudioClip attackOK;
+	public AudioClip[] attackFAIL;
+
+	public AudioClip killed;
+
+	public static void PlayAttackOK()
+	{
+		audio.clip = attackOK;
+		audio.Play ();
+	}
+	
+	public static void PlayAttackFAIL()
+	{
+		audio.clip = attackFAIL[Random.Range(0, attackFAIL.GetLength)];
+		audio.Play ();
+	}
+	
+	public static void PlayKilled()
+	{
+		audio.clip = killed;
+		audio.Play ();		
+	}
+
+}
