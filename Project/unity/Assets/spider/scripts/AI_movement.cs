7ccbce879d5ee4ae13ae95a718ff5c3d8fbfd1e8
JordiRQ <jrodriqueral92@gmail.com>
2014-10-23 09:38:38 +0200

    Improved AI behaviour

--- Project/unity/Assets/spider/scripts/AI_movement.cs
+++ Project/unity/Assets/spider/scripts/AI_movement.cs
@@ -3,9 +3,15 @@ using System.Collections;
 
 public class AI_movement : MonoBehaviour {
 	public Transform target;
-	NPCState myState;
+	public float aggro_range = 3.5f;
+	public float attack_range = 1.5f; //TODO move to NPCState
+	private NPCState myState;
 
-	// Use this for initialization
+	private const int PASSIVE = 0;
+	private const int MOVING = 1;
+	private const int ATTACKING = 2;
+	private int current_action = PASSIVE;
+		
 	void Start () {
 
 	}
@@ -17,11 +23,25 @@ public class AI_movement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		myState = this.gameObject.GetComponent<NPCState> ();
+		float dist = Vector3.Distance (transform.position, target.position);
 
-		if (Vector3.Distance (transform.position,target.position)>2) {
-			myState.setDestination (target.position.x, target.position.y, target.position.z);
-		} else {
+		if (myState.state!=NPCState.MUERTO){
+			if (dist<attack_range){
+				if (current_action==MOVING){
+					myState.setDestination (transform.position.x, transform.position.y, transform.position.z);
+				}
+				//TODO attack
+				current_action = ATTACKING;
+			}else if (dist<aggro_range){
+				if (current_action != MOVING){
+					current_action = MOVING;
+				}
+				myState.setDestination (target.position.x, target.position.y, target.position.z);
+			}
+			//If we add any blink skills on the player character, this AI would be completely screwed over...
+		}else if (current_action == MOVING){
 			myState.setDestination (transform.position.x, transform.position.y, transform.position.z);
+			current_action = PASSIVE;
 		}
 	}
 }
