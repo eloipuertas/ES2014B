361a743840dac2fdcf34fe1a054247567939fc3a
voriol <vkoriol@gmail.com>
2014-11-02 20:34:02 +0100

    #115 - script particules ini...

---
+++ Project/unity/Assets/MainCharacter/Script/Particules.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ RequireComponent( typeof( ParticleSystem ) ) ]
+
+public class Particules : MonoBehaviour 
+{
+
+	public ParticleSystem Mage;
+
+	// Use this for initialization
+	void Start () 
+	{
+		Mage.enableEmission = true;
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ( Input.GetKeyDown ( KeyCode.A ) )
+			Mage.Play();
+	
+	}
+}
