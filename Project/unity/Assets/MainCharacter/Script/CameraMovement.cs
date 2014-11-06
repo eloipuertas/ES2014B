b0dacdea5c4e18eceb894127959c8a9a0a1d91c9
Albert Folch <albertfolch58@hotmail.com>
2014-11-06 18:09:23 +0100

    issue 99, script camera millorat, ara amb zoom

--- Project/unity/Assets/MainCharacter/Script/CameraMovement.cs
+++ Project/unity/Assets/MainCharacter/Script/CameraMovement.cs
@@ -6,99 +6,112 @@ using System.Collections;
 
 public class CameraMovement : MonoBehaviour
 {
-	public float smooth = 1.5f;         // suavitat de moviment de la camera
-	public Vector3 posCamera;
+		public float smooth = 1.5f;         // suavitat de moviment de la camera
+		public Vector3 posCamera = new Vector3 (0f, 0f, 0f);
+		private Transform player;
+		private Vector3 relCameraPos;
+		private float relCameraPosMag;
+		private Vector3 newPos;
+		public int distanciaMin = 0;
+		public int distanciaMax = 8;
+		private float zoomSpeed = 4.0f;
+		private float distancia, distanciaAnt = 0;
 	
-	private Transform player;           
-	private Vector3 relCameraPos;       
-	private float relCameraPosMag;      
-	private Vector3 newPos;             
-	private float distanciaMin = 0f;
-	private float distanciaMax = 10.0f;
-	private float zoomSpeed = 1.0f;
-	private float distancia;
-
-	
-	void Awake ()
-	{
-
-		player = GameObject.FindGameObjectWithTag("Player").transform;
-		
-		posCamera = new Vector3 (-0.25f, 0.3f, -0.25f);
-		relCameraPos = transform.position - player.position;
-		relCameraPosMag = relCameraPos.magnitude; //- 0.5f;
-
-	}
-	
-	
-	void FixedUpdate ()
-	{
-
-		Vector3 standardPos = player.position + relCameraPos;
-		distancia = Mathf.Clamp (Input.GetAxis ("Mouse ScrollWheel") * zoomSpeed, distanciaMin, distanciaMax);
-
-
-		Vector3 abovePos = player.position + Vector3.up * relCameraPosMag;
-		
-
-
-		Vector3[] checkPoints = new Vector3[5];
-		
-
-		checkPoints[0] = standardPos;
-		
-
-		checkPoints[1] = Vector3.Lerp(standardPos, abovePos, 0.25f);
-		checkPoints[2] = Vector3.Lerp(standardPos, abovePos, 0.5f);
-		checkPoints[3] = Vector3.Lerp(standardPos, abovePos, 0.75f);
-		
-
-		checkPoints[4] = abovePos;
-		
-
-		for(int i = 0; i < checkPoints.Length; i++)
+		void Awake ()
 		{
-			//si la camera pot veure al player parem
-			if(ViewingPosCheck(checkPoints[i]))
 
-				break;
+				player = GameObject.FindGameObjectWithTag ("Player").transform;
+		
+				//posCamera = new Vector3 (-0.25f, 0.3f, -0.25f);
+				
+				relCameraPos = transform.position - player.position;
+				relCameraPosMag = relCameraPos.magnitude; //- 0.5f;
+
 		}
-		//Debug.Log (distancia);
+	
+		void FixedUpdate ()
+		{
 
-		transform.position = Vector3.Lerp(transform.position, newPos, smooth * Time.deltaTime)+posCamera;
-	
-		//distancia = Mathf.Clamp (distancia + Input.GetAxis ("Mouse ScrollWheel") * zoomSpeed, distanciaMin, distanciaMax);
-
-		SmoothLookAt();
-	}
-	
-	
-	bool ViewingPosCheck (Vector3 checkPos)
-	{
-		RaycastHit hit;
+				Vector3 standardPos = player.position + relCameraPos;
+				
+				Vector3 abovePos = player.position + Vector3.up * relCameraPosMag;
 		
 
-		if(Physics.Raycast(checkPos, player.position - checkPos, out hit, relCameraPosMag))
+
+				Vector3[] checkPoints = new Vector3[5];
 		
-			if(hit.transform != player)
+
+				checkPoints [0] = standardPos;
 		
-				return false;
+
+				checkPoints [1] = Vector3.Lerp (standardPos, abovePos, 0.25f);
+				checkPoints [2] = Vector3.Lerp (standardPos, abovePos, 0.5f);
+				checkPoints [3] = Vector3.Lerp (standardPos, abovePos, 0.75f);
+		
+
+				checkPoints [4] = abovePos;
+		
+
+				for (int i = 0; i < checkPoints.Length; i++) {
+						//si la camera pot veure al player parem
+						if (ViewingPosCheck (checkPoints [i]))
+
+								break;
+				}
+				//Debug.Log (distancia);
+
+				float actWheel = Input.GetAxis ("Mouse ScrollWheel");
+				
+				distancia = Mathf.Clamp (actWheel * zoomSpeed, distanciaMin, distanciaMax);
+				
+				Debug.Log (distancia);
+
+				
+				if (distanciaAnt != distancia && actWheel != 0) {
+						for (int i = 0; i < 3; i++) {
+								if (i == 1) {
+
+										posCamera [i] = distancia;
+										posCamera [i] = Mathf.Min (posCamera [i], 0.24f);
+								} else
+										posCamera [i] = 0.2f * distancia;
+								
+						}
+						
+						distanciaAnt = distancia;
+				}
+				
+		
+				transform.position = Vector3.Lerp (transform.position, newPos, smooth * Time.deltaTime) + posCamera;
+		
+				SmoothLookAt ();
+		}
+	
+		bool ViewingPosCheck (Vector3 checkPos)
+		{
+				RaycastHit hit;
+		
+
+				if (Physics.Raycast (checkPos, player.position - checkPos, out hit, relCameraPosMag))
+		
+				if (hit.transform != player)
+		
+						return false;
 		
 	
-		newPos = checkPos;
-		return true;
-	}
+				newPos = checkPos;
+				return true;
+		}
 	
+		void SmoothLookAt ()
+		{
 	
-	void SmoothLookAt ()
-	{
-	
-		Vector3 relPlayerPosition = player.position - transform.position;
+				Vector3 relPlayerPosition = player.position - transform.position;
 		
 	
-		Quaternion lookAtRotation = Quaternion.LookRotation(relPlayerPosition, Vector3.up);
-		Debug.Log (smooth * Time.deltaTime);
+				Quaternion lookAtRotation = Quaternion.LookRotation (relPlayerPosition, Vector3.up);
+				
 	
-		transform.rotation = Quaternion.Lerp(transform.rotation, lookAtRotation, smooth * Time.deltaTime);
-	}
+				transform.rotation = Quaternion.Lerp (transform.rotation, lookAtRotation, smooth * Time.deltaTime);
+		}
 }
