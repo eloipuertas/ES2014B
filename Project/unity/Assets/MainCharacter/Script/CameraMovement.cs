2ef231233c86e1d87b17f6d971b4cc4803dcae44
Cristhian CT <c.carmona.torres@gmail.com>
2014-11-08 20:16:20 +0100

    +camara sin zoom

--- Project/unity/Assets/MainCharacter/Script/CameraMovement.cs
+++ Project/unity/Assets/MainCharacter/Script/CameraMovement.cs
@@ -7,15 +7,17 @@ using System.Collections;
 public class CameraMovement : MonoBehaviour
 {
 		public float smooth = 1.5f;         // suavitat de moviment de la camera
-		public Vector3 posCamera = new Vector3 (0f, 0f, 0f);
+		private Vector3 posCamera = new Vector3 (0f, 0f, 0f);
+		public Vector3 distCamera = new Vector3 (-20f, 15f, -20f);
 		private Transform player;
 		private Vector3 relCameraPos;
 		private float relCameraPosMag;
 		private Vector3 newPos;
 		public int distanciaMin = 0;
-		public int distanciaMax = 8;
+		public int distanciaMax = 10;
 		private float zoomSpeed = 4.0f;
 		private float distancia, distanciaAnt = 0;
+		private Vector3 antigaPos = new Vector3 (0, 0, 0);
 	
 		void Awake ()
 		{
@@ -50,13 +52,15 @@ public class CameraMovement : MonoBehaviour
 		
 
 				checkPoints [4] = abovePos;
-		
+
+				
 
 				for (int i = 0; i < checkPoints.Length; i++) {
 						//si la camera pot veure al player parem
-						if (ViewingPosCheck (checkPoints [i]))
-
+						if (ViewingPosCheck (checkPoints [i])) {
+								
 								break;
+						}
 				}
 				//Debug.Log (distancia);
 
@@ -67,23 +71,44 @@ public class CameraMovement : MonoBehaviour
 				//Debug.Log (distancia);
 
 				
-				if (distanciaAnt != distancia && actWheel != 0) {
+				/*if (distanciaAnt != distancia && actWheel != 0) {
 						for (int i = 0; i < 3; i++) {
 								if (i == 1) {
 
 										posCamera [i] = distancia;
-										posCamera [i] = Mathf.Min (posCamera [i], 0.24f);
+										//posCamera [i] = Mathf.Min (posCamera [i], 0.3f);
 								} else
-										posCamera [i] = 0.2f * distancia;
+										posCamera [i] = distancia;
 								
 						}
 						
 						distanciaAnt = distancia;
-				}
+				}*/
 				
-		
-				transform.position = Vector3.Lerp (transform.position, newPos, smooth * Time.deltaTime) + posCamera;
-		
+
+				//for (int i = 0; i < 3; i++) 						Debug.Log ("posCamera[" + i + "]= " + posCamera [i]);
+				/*if (potVeure) {
+						transform.position = Vector3.Lerp (transform.position, newPos, smooth * Time.deltaTime) + posCamera + distCamera;
+				} else {
+						transform.position = Vector3.Lerp (transform.position, newPos, smooth * Time.deltaTime)-distCamera;
+				}*/
+
+				//si no el veig a +dist
+				//posar camera a transform.position = Vector3.Lerp (transform.position, newPos, smooth * Time.deltaTime)
+				//si el veig a +dist
+				//posar camera a +dist
+				
+				if (ViewingPosCheck (Vector3.Lerp (transform.position, abovePos + distCamera, smooth * Time.deltaTime))) {
+						
+						
+						//transform.position = abovePos + distCamera;
+						transform.position = Vector3.Lerp (transform.position, abovePos+distCamera, smooth * Time.deltaTime);
+						
+						
+					
+				} else {
+						transform.position = Vector3.Lerp (transform.position, newPos, smooth * Time.deltaTime);
+				}
 				SmoothLookAt ();
 		}
 	
