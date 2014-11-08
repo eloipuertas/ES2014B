02cb1d510be811114970c69d4bd92b8bd74dbe82
xavierfebrer <xavierfebrer@gmail.com>
2014-11-08 20:40:40 +0100

    Fixed spider look direction when attacking

--- Project/unity/Assets/spider/scripts/SpiderState.cs
+++ Project/unity/Assets/spider/scripts/SpiderState.cs
@@ -2,31 +2,25 @@ using UnityEngine;
 using System.Collections;
 
 public class SpiderState : AbstractEntity {
-
+	
 	public float moveSpeed = 1.5f;
 	public float rotationSpeed = 4.0f;
-
+	
 	public Vector3 destination;
-
+	
 	CharacterController characterController;
 	Animator animator;
 	
 	void Awake(){
 		characterController = GetComponent<CharacterController>();
 		animator = GetComponent<Animator>();
-
+		
 		//TODO fix attributes using D&D formulaes on the base stats
 		HP = 100;
 		DMG = 5;
-
+		
 		setDestination(transform.position.x,transform.position.y,transform.position.z);
 		
-		/*// Get the Animator component from your gameObject
-		// Sets the value
-		animator.SetBool("InConga", true); 
-		// Gets the value
-		bool isInConga = animator.GetBool("InConga");*/
-
 	}
 	
 	void Update(){
@@ -34,12 +28,12 @@ public class SpiderState : AbstractEntity {
 			move();
 		}
 	}
-
+	
 	public override void onAttackReceived (int baseDMG)
 	{
 		this.substractHealth(baseDMG);
 	}
-
+	
 	private void move(){
 		if ( animator != null && characterController != null ) { 
 			Vector3 moveDirection = destination-transform.position;
@@ -55,9 +49,9 @@ public class SpiderState : AbstractEntity {
 	
 	// LOOK
 	public void lookAt(){
-		if((destination - transform.position).magnitude < 0.1){
+		/*if((destination - transform.position).magnitude < 0.1){
 			return;
-		}
+		}*/
 		Quaternion newRotation = Quaternion.LookRotation(destination - transform.position);
 		newRotation.x = 0f;
 		newRotation.z = 0f;
@@ -79,7 +73,7 @@ public class SpiderState : AbstractEntity {
 	public Vector3 getDestination(){
 		return destination;
 	}
-
+	
 	public void setDestination(float x,float y,float z){
 		if (animator != null) {
 			if (isAlive ()) {
@@ -102,7 +96,7 @@ public class SpiderState : AbstractEntity {
 			}
 		}
 	}
-
+	
 	public void addHealth(int healthToAdd){
 		HP = HP + healthToAdd;
 	}
