493beb8053ca56cd223c1dc5f611c797b0810551
Mithrandir0x <mithrandir0x@gmail.com>
2014-11-11 21:54:45 +0100

    Finished merging devel-B to devel

--- Project/unity/Assets/spider/scripts/SpiderAI.cs
+++ Project/unity/Assets/spider/scripts/SpiderAI.cs
@@ -6,38 +6,38 @@ public class SpiderAI : MonoBehaviour {
 	public float aggroRange = 3.5f;
 	public float attackRange = 1.5f; //TODO move to NPCState
 	private SpiderState myState;
-
+	
 	private const int PASSIVE = 0;
 	private const int MOVING = 1;
 	private const int ATTACKING = 2;
 	private int currentAction = PASSIVE;
-
+	
 	private Vector3 lastTargetPos;
 	private NavMeshAgent agent;
 	private NavMeshPath path;
 	private int current_corner;
-		
+	
 	void Start () {
-
+		
 	}
 	void Awake () {
 		myState = this.gameObject.GetComponent<SpiderState> ();
 		agent = GetComponent<NavMeshAgent>();
 		agent.updatePosition = false;
 		agent.updateRotation = false;
-
+		
 		if (target == null) {
 			GameObject goTarget = getPlayerGameObject ();
 			target = goTarget.transform;
 		}
-
+		
 		path = new NavMeshPath ();
 		agent.CalculatePath(target.position, path);
 		lastTargetPos = target.position;
 		current_corner = 1;
 		InvokeRepeating ("checkPath", 0, 0.25f);
 	}
-
+	
 	private void checkPath(){
 		if (!Vector3.Equals (lastTargetPos, target.position) && currentAction==MOVING) {
 			agent.CalculatePath(target.position, path);
@@ -57,21 +57,21 @@ public class SpiderAI : MonoBehaviour {
 			**/
 		}
 	}
-
+	
 	// DebugRays are only visible in SCENE view, not in GAME view!
 	private void debugDrawPath()
 	{
 		Vector3[] corners = path.corners;
-
+		
 		if ( corners.Length >= 2 ) {
 			//Debug.Log("Drawing path, corners.Length: " + corners.Length);
-
+			
 			for ( int i = 1 ; i < corners.Length ; i++ ) {
-			//while ( i < corners.Length ) {
+				//while ( i < corners.Length ) {
 				Vector3 u = corners[i - 1];
 				Vector3 v = corners[i];
 				// UnityEngine.Debug.DrawRay(UnityEngine.Vector3, UnityEngine.Vector3, UnityEngine.Color, float, bool)
-
+				
 				if ( i % 2 != 0 ) {
 					Debug.DrawRay(v, u, Color.green, 100, true);
 				} else {
@@ -81,7 +81,7 @@ public class SpiderAI : MonoBehaviour {
 			}
 		}
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
 		//debugDrawPath();
@@ -98,21 +98,32 @@ public class SpiderAI : MonoBehaviour {
 						currentAction = ATTACKING;
 					}
 				}else if (dist<aggroRange){
-					if (Vector3.Equals(path.corners[current_corner],transform.position)){
-						current_corner++;
-					}
-					Vector3 dest = path.corners[current_corner];
-
 					if (currentAction == PASSIVE){
 						RaycastHit hit;
 						if(Physics.Raycast(transform.position, target.position-transform.position, out hit, dist)) {
 							if ((hit.point-target.position).magnitude<1){ //TODO to change when the main character fixes their tag
 								currentAction = MOVING;
+								checkPath ();
+								if (Vector3.Equals(path.corners[current_corner],transform.position)){
+									current_corner++;
+								}
+								Vector3 dest = path.corners[current_corner];
 								myState.setDestination (dest.x, dest.y, dest.z);
 							}
 						}
 					}else if (currentAction == ATTACKING){
 						currentAction = MOVING;
+						checkPath ();
+						if (Vector3.Equals(path.corners[current_corner],transform.position)){
+							current_corner++;
+						}
+						Vector3 dest = path.corners[current_corner];
+						myState.setDestination (dest.x, dest.y, dest.z);
+					}else{
+						if (Vector3.Equals(path.corners[current_corner],transform.position)){
+							current_corner++;
+						}
+						Vector3 dest = path.corners[current_corner];
 						myState.setDestination (dest.x, dest.y, dest.z);
 					}
 				}
@@ -122,8 +133,8 @@ public class SpiderAI : MonoBehaviour {
 			}
 		}
 	}
-
+	
 	private GameObject getPlayerGameObject() {
 		return GameObject.FindWithTag("Player");
-    }
+	}
 }
