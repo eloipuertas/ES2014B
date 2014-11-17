06230cd4400368bd8106fc3027e8ea8da900d8c5
voriol <vkoriol@gmail.com>
2014-11-17 22:34:12 +0100

    #136 change sounds

---
+++ Project/unity/Assets/MainCharacter/Script/FootSteps.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ RequireComponent( typeof( AudioSource ) ) ]
+
+public class FootSteps : MonoBehaviour 
+{
+	public GameObject target;
+
+	public AudioClip[] walkSounds;
+
+	// Use this for initialization
+	void Start () 
+	{
+		target = GameObject.FindGameObjectWithTag ("Player");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if ( target.transform.hasChanged && ! audio.isPlaying ) 
+		{
+			audio.clip = walkSounds[Random.Range(0, walkSounds.Length)];
+			audio.Play();
+			
+			target.transform.hasChanged = false;
+			
+		}
+	
+	}
+}
