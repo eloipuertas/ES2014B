80c96d9382feb62729d48dde29db5ba1288e5baa
xavierfebrer <xavierfebrer@gmail.com>
2014-11-19 15:24:38 +0100

    Cleaned the HealthManaBarNPC class

--- Project/unity/Assets/spider/scripts/HealthManBarNPC.cs
+++ Project/unity/Assets/spider/scripts/HealthManBarNPC.cs
@@ -5,52 +5,48 @@ public class HealthBarNPC : MonoBehaviour {
 	
 	public float maxValueHealth;
 	public float maxValueMana;
-	public float progressHealth;
-	public float progressMana;
-	public float leftX, bottomY, width, height;
+	public float currentHealth;
+	public float currentMana;
+	public float leftX,bottomY,width,height;
 		
-	public Texture2D bgImage; 
-	public Texture2D fgImage;
+	public Texture2D bgBar;
+	public Texture2D healthBarFGTexture;
+	public Texture2D manaBarFGTexture;
 
 	void Awake(){
-		set (1, 1, 1, 1, 0, 0, 200, 50);
+		set(1,1,1,1,0,0,200,50);
 	}
 
 	public void set(float maxHealth,float maxMana,float currentHealth,float currentMana,
 	                float leftX,float bottomY,float width,float height){
 		this.maxValueHealth = maxHealth;
 		this.maxValueMana = maxMana;
-		this.progressHealth = currentHealth;
-		this.progressMana = currentMana;
+		this.currentHealth = currentHealth;
+		this.currentsMana = currentMana;
 		this.leftX = leftX;
 		this.bottomY = bottomY;
 		this.width = width;
 		this.height = height;
 	}
-	
-	// Update is called once per frame
+
 	void Update(){
-		AddjustCurrentHealth(0);
 	}
 	
-	void OnGUI () {
-		// Create one Group to contain both images
-		// Adjust the first 2 coordinates to place it somewhere else on-screen
-		GUI.BeginGroup (new Rect (this.leftX,this.bottomY, healthBarLength,32));
-		
-		// Draw the background image
-		GUI.Box (new Rect (this.leftX,this.bottomY, healthBarLength,32), bgImage);
-		
-			// Create a second Group which will be clipped
-			// We want to clip the image and not scale it, which is why we need the second Group
-			GUI.BeginGroup (new Rect (this.leftX,this.bottomY0, curHealth / maxHealth * healthBarLength, 32));
+	void OnGUI(){
+		// container
+		GUI.BeginGroup(new Rect(this.leftX,this.bottomY,this.width,this.height));
+		GUI.Box(new Rect(this.leftX,this.bottomY,this.width,this.height),bgBar);
 			
-			// Draw the foreground image
-			GUI.Box (new Rect (this.leftX,this.bottomY,healthBarLength,32), fgImage);
-			
-			// End both Groups
-			GUI.EndGroup ();
+			// health container
+			GUI.BeginGroup(new Rect(this.leftX,this.bottomY+this.height*0.5f,(this.currentHealth/this.maxValueHealth)*this.width,this.height*0.5f));
+			GUI.Box(new Rect(tthis.leftX,this.bottomY+this.height*0.5f,(this.currentHealth/this.maxValueHealth)*this.width,this.height*0.5f),healthBarFGTexture);
+			GUI.EndGroup();
 		
-		GUI.EndGroup ();
+			// mana container
+			GUI.BeginGroup(new Rect(this.leftX,this.bottomY,(this.currentMana/this.maxValueMana)*this.width,this.height*0.5f));
+			GUI.Box(new Rect(tthis.leftX,this.bottomY,(this.currentMana/this.maxValueMana)*this.width,this.height*0.5f),manaBarFGTexture);
+			GUI.EndGroup();
+
+		GUI.EndGroup();
 	}
 }
