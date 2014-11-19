437ae2c3fc699771827f4551fc8f9b836e4b3c2c
xavierfebrer <xavierfebrer@gmail.com>
2014-11-19 16:12:34 +0100

    Es mostra per pantalla pero git reset

--- Project/unity/Assets/spider/scripts/HealthManBarNPC.cs
+++ Project/unity/Assets/spider/scripts/HealthManBarNPC.cs
@@ -1,19 +1,21 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class HealthManBarNPC : MonoBehaviour {
+public class HealthManBarNPC:MonoBehaviour{
 	
+	private SpiderState myState;
 	public float maxValueHealth;
 	public float maxValueMana;
 	public float currentHealth;
 	public float currentMana;
 	public float leftX,bottomY,width,height;
 		
-	public Texture2D bgBar;
+	public Texture2D bgBarTexture;
 	public Texture2D healthBarFGTexture;
 	public Texture2D manaBarFGTexture;
-
+	
 	void Awake(){
+		myState = this.gameObject.GetComponent<SpiderState>();
 		set(1,1,1,1,0,0,200,50);
 	}
 
@@ -22,33 +24,32 @@ public class HealthManBarNPC : MonoBehaviour {
 		this.maxValueHealth = maxHealth;
 		this.maxValueMana = maxMana;
 		this.currentHealth = currentHealth;
-		this.currentsMana = currentMana;
+		this.currentMana = currentMana;
 		this.leftX = leftX;
 		this.bottomY = bottomY;
 		this.width = width;
 		this.height = height;
 	}
-
+	
 	void Update(){
 		// TODO ?
+		//myState.transform.position
+		//Vector3 pos = myState.getPosition();
+		Vector3 screenPos = Camera.main.WorldToScreenPoint(myState.transform.position);
+		//print("target is " + screenPos.x + " pixels from the left");
+		this.leftX = screenPos.x/*-this.width*0.5f*/;
+		this.bottomY = screenPos.y/*-this.height*0.5f*/;
 	}
 	
 	void OnGUI(){
 		// BG container
-		GUI.BeginGroup(new Rect(this.leftX,this.bottomY,this.width,this.height));
-		GUI.Box(new Rect(this.leftX,this.bottomY,this.width,this.height),bgBar);
-			
-			// health FG container
-			GUI.BeginGroup(new Rect(this.leftX,this.bottomY+this.height*0.5f,(this.currentHealth/this.maxValueHealth)*this.width,this.height*0.5f));
-			GUI.Box(new Rect(tthis.leftX,this.bottomY+this.height*0.5f,(this.currentHealth/this.maxValueHealth)*this.width,this.height*0.5f),healthBarFGTexture);
-			GUI.EndGroup();
-		
-			// mana FG container
-			GUI.BeginGroup(new Rect(this.leftX,this.bottomY,(this.currentMana/this.maxValueMana)*this.width,this.height*0.5f));
-			GUI.Box(new Rect(tthis.leftX,this.bottomY,(this.currentMana/this.maxValueMana)*this.width,this.height*0.5f),manaBarFGTexture);
-			GUI.EndGroup();
+		GUI.DrawTexture(new Rect(this.leftX,this.bottomY,this.width,this.height),bgBarTexture);
 
-		GUI.EndGroup();
+		// health FG container
+		GUI.DrawTexture(new Rect(this.leftX,this.bottomY,this.width,this.height*0.5f),healthBarFGTexture);
+
+		// mana FG container
+		GUI.DrawTexture(new Rect(this.leftX,this.bottomY+this.height*0.5f,this.width,this.height*0.5f),manaBarFGTexture);
 	}
 	
 	public void setHealth(float health){
@@ -63,7 +64,7 @@ public class HealthManBarNPC : MonoBehaviour {
 		this.maxValueHealth = maxHealth;
 	}
 	
-	public void setMana(float maxMana){
+	public void setMaxMana(float maxMana){
 		this.maxValueMana = maxMana;
 	}
 	
