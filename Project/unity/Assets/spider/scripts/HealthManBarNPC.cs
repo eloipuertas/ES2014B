9a89101380eae5411b05c516e760908bdc20b402
xavierfebrer <xavierfebrer@gmail.com>
2014-11-19 16:31:07 +0100

    Added the visible healthbar to the playground

--- Project/unity/Assets/spider/scripts/HealthManBarNPC.cs
+++ Project/unity/Assets/spider/scripts/HealthManBarNPC.cs
@@ -8,7 +8,7 @@ public class HealthManBarNPC:MonoBehaviour{
 	public float maxValueMana = 100;
 	public float currentHealth = 100;
 	public float currentMana = 100;
-	public float leftX=0,bottomY=0,width=200,height=50;
+	public float leftX=0,topY=0,width=100,height=25;
 		
 	public Texture2D bgBarTexture;
 	public Texture2D healthBarFGTexture;
@@ -16,41 +16,41 @@ public class HealthManBarNPC:MonoBehaviour{
 	
 	void Awake(){
 		myState = this.gameObject.GetComponent<SpiderState>();
-		set(myState.getMAXHP(),myState.getMAXMP(),myState.getHP(),myState.getMP(),leftX,bottomY,width,height);
+		set(myState.getMAXHP(),myState.getMAXMP(),myState.getHP(),myState.getMP(),leftX,topY,width,height);
 	}
 
 	public void set(float maxHealth,float maxMana,float currentHealth,float currentMana,
-	                float leftX,float bottomY,float width,float height){
+	                float leftX,float topY,float width,float height){
 		this.maxValueHealth = maxHealth;
 		this.maxValueMana = maxMana;
 		this.currentHealth = currentHealth;
 		this.currentMana = currentMana;
 		this.leftX = leftX;
-		this.bottomY = bottomY;
+		this.topY = topY;
 		this.width = width;
 		this.height = height;
 	}
 	
 	void Update(){
 		// TODO ?
-		set(myState.getMAXHP(),myState.getMAXMP(),myState.getHP(),myState.getMP(),leftX,bottomY,width,height);
+		set(myState.getMAXHP(),myState.getMAXMP(),myState.getHP(),myState.getMP(),leftX,topY,width,height);
 		//myState.transform.position
 		//Vector3 pos = myState.getPosition();
 		Vector3 screenPos = Camera.main.WorldToScreenPoint(myState.transform.position);
 		//print("target is " + screenPos.x + " pixels from the left");
-		this.leftX = screenPos.x/*-this.width*0.5f*/;
-		this.bottomY = screenPos.y/*-this.height*0.5f*/;
+		this.leftX = screenPos.x-this.width*0.5f;
+		this.topY = Screen.height-screenPos.y-this.height*0.5f;
 	}
 	
 	void OnGUI(){
 		// BG container
-		GUI.DrawTexture(new Rect(this.leftX,this.bottomY,this.width,this.height),bgBarTexture);
+		GUI.DrawTexture(new Rect(this.leftX,this.topY,this.width,this.height),bgBarTexture);
 
 		// health FG container
-		GUI.DrawTexture(new Rect(this.leftX,this.bottomY,(this.currentHealth/this.maxValueHealth)*this.width,this.height*0.5f),healthBarFGTexture);
+		GUI.DrawTexture(new Rect(this.leftX,this.topY,(this.currentHealth/this.maxValueHealth)*this.width,this.height*0.5f),healthBarFGTexture);
 
 		// mana FG container
-		GUI.DrawTexture(new Rect(this.leftX,this.bottomY+this.height*0.5f,(this.currentMana/this.maxValueMana)*this.width,this.height*0.5f),manaBarFGTexture);
+		GUI.DrawTexture(new Rect(this.leftX,this.topY+this.height*0.5f,(this.currentMana/this.maxValueMana)*this.width,this.height*0.5f),manaBarFGTexture);
 	}
 	
 	public void setHealth(float health){
@@ -73,8 +73,8 @@ public class HealthManBarNPC:MonoBehaviour{
 		this.leftX = leftX;
 	}
 	
-	public void setBottomY(float bottomY){
-		this.bottomY = bottomY;
+	public void setTopY(float topY){
+		this.topY = topY;
 	}
 	
 	public void setWidth(float width){
