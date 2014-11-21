e049a906f7c0c60e02badadf23d0bbbfb8b2c69c
Albert Folch <albertfolch58@hotmail.com>
2014-11-21 19:42:46 +0100

    issue 125 acabada

--- Project/unity/Assets/Menu/Script/HUD.cs
+++ Project/unity/Assets/Menu/Script/HUD.cs
@@ -6,178 +6,185 @@ public class HUD : MonoBehaviour
 	
 	
 	
-		private float amplada;
-		private float altura;
+	private float amplada;
+	private float altura;
 	
-		//atributos de la vida
-		public Texture texVida;
-		private float vida, alturaVida, vidapercent;
+	//atributos de la vida
+	public Texture texVida;
+	private float vida, alturaVida, vidapercent;
 
 	
-		//atributos del mana
-		public Texture texMana;
-		private float mana, alturaMana, manapercent;
-		public Texture[] magicTextures = new Texture[3];
-		public Texture[] magicTexturesSpelled = new Texture[3];
+	//atributos del mana
+	public Texture texMana;
+	private float mana, alturaMana, manapercent;
+	public Texture[] magicTextures = new Texture[3];
+	public Texture[] magicTexturesSpelled = new Texture[3];
 		
-		//textura que saldra cuando el personaje muera
-		public Texture gameOverTexture;
-		private float xPos;
-		private float yPos;
-		private MainPjMovement pj;
-		private Player player;
-		private float buttonSizeWidth, buttonSizeHeight;
-		public GUISkin myskin;
-		public Texture backgroundHud;
-		public Texture continueTexture, backMainMenuTexture, audioOFF, audioON;
-		private Texture audioTexture;
-		private bool sona = true;
-		private AmbientalMusic AmbientAudio;
-		private int  magiaEscollida = -1;
+	//textura que saldra cuando el personaje muera
+	public Texture gameOverTexture;
+	private float xPos;
+	private float yPos;
+	private MainPjMovement pj;
+	private Player player;
+	private float buttonSizeWidth, buttonSizeHeight;
+	public GUISkin myskin;
+	public Texture backgroundHud;
+	public Texture continueTexture, backMainMenuTexture, audioOFF, audioON;
+	private Texture audioTexture;
+	private bool sona = true;
+	private AmbientalMusic AmbientAudio;
+	private int  magiaEscollida = -1;
+	private float timeLeft = 2f;
 
-		void Start ()
-		{
+	void Start ()
+	{
 
-				GameObject go = GameObject.FindGameObjectWithTag ("Player");
-				pj = go.GetComponent ("MainPjMovement") as MainPjMovement;
-				player = go.GetComponent ("Player") as Player;
+		GameObject go = GameObject.FindGameObjectWithTag ("Player");
+		pj = go.GetComponent ("MainPjMovement") as MainPjMovement;
+		player = go.GetComponent ("Player") as Player;
 		
-				audioTexture = audioON;
-				AmbientAudio = GameObject.FindObjectOfType (typeof(AmbientalMusic)) as AmbientalMusic;
+		audioTexture = audioON;
+		AmbientAudio = GameObject.FindObjectOfType (typeof(AmbientalMusic)) as AmbientalMusic;
 		
 		
-		}
+	}
 	
-		void OnGUI ()
-		{
-				Time.timeScale = 1;
-				amplada = Screen.width / 10;
-				altura = Screen.height / 8;
-				xPos = Screen.width / 2.7f;
-				yPos = Screen.height / 3.2f;
+	void OnGUI ()
+	{
+		Time.timeScale = 1;
+		amplada = Screen.width / 10;
+		altura = Screen.height / 8;
+		xPos = Screen.width / 2.7f;
+		yPos = Screen.height / 3.2f;
 				
-				GUI.skin = myskin;
+		GUI.skin = myskin;
 
-				//GUI.DrawTexture (new Rect (100,478,700,120),backgroundHud);
+		//GUI.DrawTexture (new Rect (100,478,700,120),backgroundHud);
 
-				buttonSizeHeight = Screen.height / 15;
-				buttonSizeWidth = Screen.width / 5;
-				float maxVida, maxMana;
+		buttonSizeHeight = Screen.height / 15;
+		buttonSizeWidth = Screen.width / 5;
+		float maxVida, maxMana;
 				
 
-				vida = pj.getHP ();
-				mana = pj.getMP ();
-				magiaEscollida = pj.getSelectedSpell ();
-				maxVida = pj.getMAXHP ();
-				maxMana = pj.getMAXMP ();
+		vida = pj.getHP ();
+		mana = pj.getMP ();
+		magiaEscollida = pj.getSelectedSpell ();
+		maxVida = pj.getMAXHP ();
+		maxMana = pj.getMAXMP ();
 
 
 				
 		        
 				
-				vidapercent = vida / maxVida;
+		vidapercent = vida / maxVida;
 				
 
-				if (vidapercent < 0)
-						vidapercent = 0;
-				if (vidapercent > 100)
-						vidapercent = 100;
+		if (vidapercent < 0)
+			vidapercent = 0;
+		if (vidapercent > 100)
+			vidapercent = 100;
 		
-				alturaVida = vidapercent * altura;
+		alturaVida = vidapercent * altura;
 
 				
-				manapercent = mana / maxMana;
+		manapercent = mana / maxMana;
 
-				if (manapercent < 0)
-						manapercent = 0;
-				if (manapercent > 100)
-						manapercent = 100;
+		if (manapercent < 0)
+			manapercent = 0;
+		if (manapercent > 100)
+			manapercent = 100;
 				
-				alturaMana = manapercent * altura;
+		alturaMana = manapercent * altura;
 				
-				float xVida = Screen.width / 10;
-				float yVida = Screen.height - alturaVida;
-				float yMana = Screen.height - alturaMana;
-				float xMana = Screen.width - Screen.width * 2 / 10;
-				float xActual = xVida + amplada;
-				float alturaMagia = Screen.height / 15;
-				float yMagies = Screen.height - alturaMagia;
+		float xVida = Screen.width / 10;
+		float yVida = Screen.height - alturaVida;
+		float yMana = Screen.height - alturaMana;
+		float xMana = Screen.width - Screen.width * 2 / 10;
+		float xActual = xVida + amplada;
+		float alturaMagia = Screen.height / 15;
+		float yMagies = Screen.height - alturaMagia;
 
 
-				int numTextures = magicTextures.Length + 2;
+		int numTextures = magicTextures.Length + 2;
 
-				for (int i = 0; i < numTextures; i++) {
+		for (int i = 0; i < numTextures; i++) {
 						
 						
-						if (i == 0) {//vida
-								GUI.BeginGroup (new Rect (xVida, yVida, amplada, Screen.height - yVida));
+			if (i == 0) {//vida
+				GUI.BeginGroup (new Rect (xVida, yVida, amplada, Screen.height - yVida));
 								
-								GUI.DrawTexture (new Rect (0, alturaVida - altura, amplada, altura), this.texVida);
-								GUI.EndGroup ();
+				GUI.DrawTexture (new Rect (0, alturaVida - altura, amplada, altura), this.texVida);
+				GUI.EndGroup ();
 
-						} else if (i == numTextures - 1) {//mana
-								GUI.BeginGroup (new Rect (xMana, yMana, amplada, Screen.height - yMana));
-								GUI.DrawTexture (new Rect (0, alturaMana - altura, amplada, altura), this.texMana);
-								GUI.EndGroup ();
+			} else if (i == numTextures - 1) {//mana
+				GUI.BeginGroup (new Rect (xMana, yMana, amplada, Screen.height - yMana));
+				GUI.DrawTexture (new Rect (0, alturaMana - altura, amplada, altura), this.texMana);
+				GUI.EndGroup ();
 
-						} else {//altres
-								i--;
+			} else {//altres
+				i--;
 
-								//si hi ha 3 magies anira de 0,1,2
-								Texture texturaMagia = magiaEscollida == i ? this.magicTexturesSpelled [i] : this.magicTextures [i];
+				//si hi ha 3 magies anira de 0,1,2
+				Texture texturaMagia = magiaEscollida == i ? this.magicTexturesSpelled [i] : this.magicTextures [i];
 				
-								GUI.BeginGroup (new Rect (xActual, yMagies, alturaMagia, alturaMagia));
-								GUI.DrawTexture (new Rect (0, 0, alturaMagia, alturaMagia), texturaMagia);
-								GUI.EndGroup ();
-								i++;
-						}
+				GUI.BeginGroup (new Rect (xActual, yMagies, alturaMagia, alturaMagia));
+				GUI.DrawTexture (new Rect (0, 0, alturaMagia, alturaMagia), texturaMagia);
+				GUI.EndGroup ();
+				i++;
+			}
 						
 						
-						xActual += alturaMagia;
-				}
+			xActual += alturaMagia;
+		}
 			
 				
 				
-				if (player.canShowMenuPause () && pj.isAlive ()) { 
-						Time.timeScale = 0;
-						if (GUI.Button (new Rect (xPos, yPos, buttonSizeWidth, buttonSizeHeight), continueTexture)) {
-								player.hideMenuPause ();	
+		if (player.canShowMenuPause () && pj.isAlive ()) { 
+			Time.timeScale = 0;
+			if (GUI.Button (new Rect (xPos, yPos, buttonSizeWidth, buttonSizeHeight), continueTexture)) {
+				player.hideMenuPause ();	
 
 				
-						}
-						if (GUI.Button (new Rect (xPos, buttonSizeHeight + yPos, buttonSizeWidth, buttonSizeHeight), audioTexture)) {
+			}
+			if (GUI.Button (new Rect (xPos, buttonSizeHeight + yPos, buttonSizeWidth, buttonSizeHeight), audioTexture)) {
 
-								if (sona) {//pausar audio
-										AmbientAudio.PauseAudio ();
-										audioTexture = audioOFF;
-								} else {//reproduir audio
-										AmbientAudio.UnPauseAudio ();
-										audioTexture = audioON;
+				if (sona) {//pausar audio
+					AmbientAudio.PauseAudio ();
+					audioTexture = audioOFF;
+				} else {//reproduir audio
+					AmbientAudio.UnPauseAudio ();
+					audioTexture = audioON;
 										
-								}
-								sona = !sona;
-				
-						}
-						if (GUI.Button (new Rect (xPos, 2 * buttonSizeHeight + yPos, buttonSizeWidth, buttonSizeHeight), backMainMenuTexture)) {
-								Destroy (this.gameObject);
-								Object.Destroy (GameObject.FindGameObjectWithTag ("Player"));
-								Application.LoadLevel (0);
-							
-						}
-				} 
-
-				if (vida <= 0) {
-						AmbientAudio.PlayGameOver ();
-						GUI.Label (new Rect (xPos, yPos, Screen.width / 3, Screen.height / 3), gameOverTexture);
-						if (GUI.Button (new Rect (xPos * 1.1f, 2f * yPos, buttonSizeWidth, buttonSizeHeight), backMainMenuTexture)) {
-								Destroy (this.gameObject);
-								Application.LoadLevel (0);
-				
-						}
 				}
-		
-		
-		
+				sona = !sona;
+				
+			}
+			if (GUI.Button (new Rect (xPos, 2 * buttonSizeHeight + yPos, buttonSizeWidth, buttonSizeHeight), backMainMenuTexture)) {
+				Destroy (this.gameObject);
+				Object.Destroy (GameObject.FindGameObjectWithTag ("Player"));
+				Application.LoadLevel (0);
+							
+			}
+		} 
+
+		if (vida <= 0) {
+			timeLeft -= Time.deltaTime;
+			if (timeLeft < 0) {
+
+				AmbientAudio.PlayGameOver ();
+				GUI.Label (new Rect (xPos, 0, gameOverTexture.width, gameOverTexture.height), gameOverTexture);
+				Time.timeScale = 0;
+				if (GUI.Button (new Rect (xPos+gameOverTexture.width*0.01f, gameOverTexture.height, gameOverTexture.width, gameOverTexture.width/2), backMainMenuTexture)) {
+					Destroy (this.gameObject);
+					Object.Destroy (GameObject.FindGameObjectWithTag ("Player"));
+					Application.LoadLevel (0);
+				
+				}
+			}
 		}
+		
+		
+		
+	}
 		
 }
