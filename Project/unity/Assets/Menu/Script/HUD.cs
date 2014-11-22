954adf6cf9d8c435fc87adb2162748edf3bf3498
Albert Folch <albertfolch58@hotmail.com>
2014-11-22 16:26:13 +0100

    hud millorat

--- Project/unity/Assets/Menu/Script/HUD.cs
+++ Project/unity/Assets/Menu/Script/HUD.cs
@@ -17,12 +17,12 @@ public class HUD : MonoBehaviour
 	//atributos del mana
 	public Texture texMana,manaCover;
 	private float mana, alturaMana, manapercent;
-	public Texture[] magicTextures = new Texture[1];
-	public Texture[] magicTexturesSpelled = new Texture[1];
+	public Texture[] magicTextures = new Texture[3];
+	public Texture[] magicTexturesSpelled = new Texture[3];
 
 	public Texture texEscut;
 	//textura que saldra cuando el personaje muera
-	public Texture gameOverTexture,hud_bg;
+	public Texture gameOverTexture,hud_bg,magiaSelect,magiaNormal;
 	private float xPos;
 	private float yPos;
 	private MainPjMovement pj;
@@ -46,6 +46,18 @@ public class HUD : MonoBehaviour
 		
 		audioTexture = audioON;
 		AmbientAudio = GameObject.FindObjectOfType (typeof(AmbientalMusic)) as AmbientalMusic;
+
+		string playerSelected = PlayerPrefs.GetString ("player");
+		if (playerSelected.Equals("player1")) {
+			magiaNormal = magicTextures[0];
+			magiaSelect = magicTexturesSpelled[0];
+		}else if(playerSelected.Equals("player2")){
+			magiaNormal = magicTextures[1];
+			magiaSelect = magicTexturesSpelled[1];
+		}else{
+			magiaNormal = magicTextures[2];
+			magiaSelect = magicTexturesSpelled[2];
+		}
 		
 		
 	}
@@ -59,8 +71,6 @@ public class HUD : MonoBehaviour
 		yPos = Screen.height / 3.2f;
 				
 		GUI.skin = myskin;
-
-		//GUI.DrawTexture (new Rect (100,478,700,120),backgroundHud);
 
 		buttonSizeHeight = Screen.height / 15;
 		buttonSizeWidth = Screen.width / 5;
@@ -93,21 +103,24 @@ public class HUD : MonoBehaviour
 			manapercent = 0;
 		if (manapercent > 100)
 			manapercent = 100;
-				
+		manapercent = 100;//borrar
 		alturaMana = manapercent * altura;
 				
-		float xVida = Screen.width*0.25f;
+		float xVida = Screen.width*0.31f;
+		float xBG = Screen.width*0.25f;
 		float yVida = Screen.height - alturaVida;
 		float yMana = Screen.height - alturaMana;
-		float xMana = Screen.width - Screen.width * 2 / 10;
+		float ampladaBG = Screen.width/2;
+		//float xMana = Screen.width - Screen.width * 2 / 10;
+		float xMana = xBG+ampladaBG-amplada-Screen.width*0.06f;
 		float xActual = xVida + amplada;
 		float alturaMagia = Screen.height / 15;
-		float yMagies = Screen.height - alturaMagia;
+		float yMagies = Screen.height - alturaMagia -Screen.height*0.01f;
 
 
-		int numTextures = magicTextures.Length + 2;
-		float ampladaBG = Screen.width/2;
-		GUI.DrawTexture (new Rect (xVida, Screen.height-Screen.height / 8,ampladaBG,Screen.height / 8),this.hud_bg);
+		int numTextures = 3;
+
+		GUI.DrawTexture (new Rect (xBG, Screen.height-Screen.height / 5,ampladaBG,Screen.height / 5),this.hud_bg);
 		for (int i = 0; i < numTextures; i++) {
 						
 
@@ -119,27 +132,26 @@ public class HUD : MonoBehaviour
 				GUI.EndGroup ();
 			} else if (i == numTextures - 1) {//mana
 				//GUI.BeginGroup (new Rect (xMana, yMana, amplada, Screen.height - yMana));
-				GUI.BeginGroup (new Rect (xVida+ampladaBG-amplada, yMana, amplada, Screen.height - yMana));
+
+				GUI.BeginGroup (new Rect (xMana, yMana, amplada, Screen.height - yMana));
 				GUI.DrawTexture (new Rect (0, alturaMana - altura, amplada, altura), this.texMana);
 				GUI.DrawTexture (new Rect (0, alturaMana - altura, amplada, altura), this.manaCover);
 				GUI.EndGroup ();
 			} else {//altres
-				i--;
 
-				//si hi ha 3 magies anira de 0,1,2
-				Texture texturaMagia = magiaEscollida == i ? this.magicTexturesSpelled [i] : this.magicTextures [i];
+				Texture texturaMagia = magiaEscollida == i ? magiaNormal : magiaSelect;
 				GUI.DrawTexture (new Rect (xActual, yMagies, alturaMagia, alturaMagia), texturaMagia);
 
-				i++;
+
 			}
 						
 						
 			xActual += alturaMagia;
 		}
+		//if(pj.getShield())
+		GUI.DrawTexture (new Rect (xMana-alturaMagia-Screen.width*0.01f, yMagies, alturaMagia, alturaMagia), texEscut);//descomentar a devel
 
-		GUI.DrawTexture (new Rect (xVida+ampladaBG-amplada-alturaMagia-10, yMagies, alturaMagia, alturaMagia), texEscut);
-			
-				
+
 				
 		if (player.canShowMenuPause () && pj.isAlive ()) { 
 			Time.timeScale = 0;
