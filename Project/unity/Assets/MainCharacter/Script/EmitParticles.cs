55eb1c5d8a0e789a691362cb5adcadc7871726b3
voriol <vkoriol@gmail.com>
2014-11-22 15:36:36 +0100

    #115 - modificacio script particles

--- Project/unity/Assets/MainCharacter/Script/EmitParticles.cs
+++ Project/unity/Assets/MainCharacter/Script/EmitParticles.cs
@@ -2,36 +2,43 @@
 using System.Collections;
 
 [ RequireComponent( typeof( ParticleSystem ) ) ]
-[ RequireComponent( typeof( AudioSource ) ) ]
 
 public class EmitParticles : MonoBehaviour 
 {
-	Vector3 pos = new Vector3 ( 0, 13, 0 );
-	Vector3 vel = new Vector3 ( 0, 0, 70 );
-	float siz = 3;
-	float lif = 5;
-	Color32 col = Color.white;
+	public Vector3 pos = new Vector3 ( 0, 13, 0 ); // Setejar a la posicio de la ma
+	public Vector3 vel = new Vector3();
 
-	public AudioClip[] fire_ball;
-	
-	// Use this for initialization
-	void Start () 
-	{
-		particleSystem.playOnAwake = false;
+	public int velocitat = 1;
 
-	}
+	public float siz = 3;
+	public float lif = 5;
+
+	public Color32 col = Color.white;
 	
-	// Update is called once per frame
-	void Update () 
-	{
-		if ( Input.GetKeyDown ( KeyCode.A ) ) 
-		{
+	public AudioClip fire_ball;
+
+	GameObject player = GameObject.FindWithTag("Player");
+	GameObject target = GameObject.FindWithTag("PNJ");
+	
+	// public void throwParticle( GameObject player, GameObject target ) 
+	public void throwParticle()
+	{		
+		// Definim origen de la particula
+		transform.position = player.transform.position;
+		transform.rotation = player.transform.rotation;
+		
+		// Definim la velocitat ( direccio de la bola ) a partir del target
+		vel [0] = (target.transform.position.x - player.transform.position.x) * velocitat;
+		vel [1] = (target.transform.position.y - player.transform.position.y) * velocitat;
+		vel [2] = (target.transform.position.z - player.transform.position.z) * velocitat;
+		
+		if( ! particleSystem.isPlaying )
 			particleSystem.Emit ( pos, vel, siz, lif, col );
-
-			audio.clip = fire_ball[Random.Range(0, fire_ball.Length)];
-			audio.Play ();
-
-		}
+		
+		audio.clip = fire_ball;
+		audio.Play ();
 
 	}
+	
 }
+
