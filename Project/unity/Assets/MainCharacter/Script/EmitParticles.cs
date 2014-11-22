b40b9c836b795f9d71fc011542d545717857d724
voriol <vkoriol@gmail.com>
2014-11-22 18:07:36 +0100

    #C-115- canvis al script EmitParticles.cs

--- Project/unity/Assets/MainCharacter/Script/EmitParticles.cs
+++ Project/unity/Assets/MainCharacter/Script/EmitParticles.cs
@@ -9,7 +9,7 @@ public class EmitParticles : MonoBehaviour
 	public Vector3 pos = new Vector3 ( 0, 13, 0 ); // Setejar a la posicio de la ma
 	private Vector3 vel = new Vector3();
 
-	public int velocitat = 1000;
+	public int velocitat = 70;
 
 	public float siz = 3;
 	public float lif = 5;
