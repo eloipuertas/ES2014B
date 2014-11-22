bdc19c7a91c84fe871a69bbccce9e15befafefe8
xavierfebrer <xavierfebrer@gmail.com>
2014-11-22 20:00:14 +0100

    Added everything up to date

--- Project/unity/Assets/Menu/Script/main_newgame_button.cs
+++ Project/unity/Assets/Menu/Script/main_newgame_button.cs
@@ -1,18 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class main_newgame_button : MonoBehaviour {
+public class main_newgame_button : MonoBehaviour{
 	
+	private static float MAX_COLOR_VAL = 0.5f;
+	public float secondsToAppear = 3.0f;
+	public float delayToAppear = 2.0f;
 	public Texture2D textureS;
 	public Texture2D textureUnS;
+	public Color color;
 	
-	void Awake(){
+	void Start(){
 		Rect initPixelInset = new Rect(0,0,1,1);
 		initPixelInset.width = Screen.width*0.20f;
 		initPixelInset.height = initPixelInset.width/3f;
 		initPixelInset.x = 0-initPixelInset.width*0.5f;
 		initPixelInset.y = -Screen.height*0.25f;
 		guiTexture.pixelInset = initPixelInset;
+		color = guiTexture.color;
+		color.a = 0;
+		guiTexture.color = color;
+	}
+
+	void OnGUI(){
+		delayToAppear = Mathf.Max(0,delayToAppear-Time.deltaTime);
+		if(delayToAppear <= 0){
+			color = guiTexture.color;
+			color.a = Mathf.Min(1,color.a+(MAX_COLOR_VAL/secondsToAppear)*Time.deltaTime);
+			guiTexture.color = color;
+		}
 	}
 	
 	public void OnMouseExit(){
--- Project/unity/Assets/Menu/Script/main_title.cs
+++ Project/unity/Assets/Menu/Script/main_title.cs
@@ -2,10 +2,14 @@
 using System.Collections;
 
 public class main_title : MonoBehaviour {
-		
-	public Texture2D texture;
 	
-	void Awake(){
+	private static float MAX_COLOR_VAL = 0.5f;
+	public float secondsToAppear = 2.0f;
+	public float delayToAppear = 1.0f;
+	public Texture2D texture;
+	public Color color;
+	
+	void Start(){
 		guiTexture.texture = texture;
 
 		Rect initPixelInset = new Rect(0,0,1,1);
@@ -14,5 +18,17 @@ public class main_title : MonoBehaviour {
 		initPixelInset.x = 0-initPixelInset.width*0.5f;
 		initPixelInset.y = 0-initPixelInset.height*0.05f;
 		guiTexture.pixelInset = initPixelInset;
+		color = guiTexture.color;
+		color.a = 0;
+		guiTexture.color = color;
+	}
+	
+	void OnGUI(){
+		delayToAppear = Mathf.Max(0,delayToAppear-Time.deltaTime);
+		if(delayToAppear <= 0){
+			color = guiTexture.color;
+			color.a = Mathf.Min(1,color.a+(MAX_COLOR_VAL/secondsToAppear)*Time.deltaTime);
+			guiTexture.color = color;
+		}
 	}
 }
