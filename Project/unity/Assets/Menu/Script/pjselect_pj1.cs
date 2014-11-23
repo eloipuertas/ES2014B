2f42e96a6a58a9456a7a9423063e291e59966932
xavierfebrer <xavierfebrer@gmail.com>
2014-11-23 01:04:49 +0100

    1/3 last textures left, gameover title updated, paused and main title left

---
+++ Project/unity/Assets/Menu/Script/pjselect_pj1.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pjselect_pj1 : MonoBehaviour{
+	
+	private static float MAX_COLOR_VAL = 0.5f;
+	private float secondsToAppear = 1.0f;
+	private float delayToAppear = 0.5f;
+	public Texture2D texture;
+	private Color color;
+	
+	void Awake(){
+		Time.timeScale = 1;
+		guiTexture.texture = texture;
+		
+		Rect initPixelInset = new Rect(0,0,1,1);
+		initPixelInset.width = Screen.width*0.20f;
+		initPixelInset.height = Screen.height*0.50f;
+		initPixelInset.x = -Screen.width*0.375f;
+		initPixelInset.y = -Screen.height*0.20f;
+		guiTexture.pixelInset = initPixelInset;
+		color = guiTexture.color;
+		color.a = 0;
+		guiTexture.color = color;
+	}
+	
+	void Update(){
+		delayToAppear = Mathf.Max(0,delayToAppear-Mathf.Abs(Time.deltaTime));
+		if(delayToAppear <= 0){
+			color = guiTexture.color;
+			color.a = Mathf.Min(1,color.a+(MAX_COLOR_VAL/secondsToAppear)*Time.deltaTime);
+			guiTexture.color = color;
+		}
+	}
+}
