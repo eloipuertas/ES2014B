e04e7601b530688e9ed597b29a2d09230697b01c
xavierfebrer <xavierfebrer@gmail.com>
2014-11-23 02:50:31 +0100

    Every new required texture for this sprint is complete

--- Project/unity/Assets/Menu/Script/main_title.cs
+++ Project/unity/Assets/Menu/Script/main_title.cs
@@ -14,7 +14,7 @@ public class main_title : MonoBehaviour {
 		guiTexture.texture = texture;
 
 		Rect initPixelInset = new Rect(0,0,1,1);
-		initPixelInset.height = Screen.height*0.40f;	// 300
+		initPixelInset.height = Screen.height*0.60f;	// 300
 		initPixelInset.width = initPixelInset.height*2f;	// 600
 		initPixelInset.x = 0-initPixelInset.width*0.5f;
 		initPixelInset.y = 0-initPixelInset.height*0.05f;
