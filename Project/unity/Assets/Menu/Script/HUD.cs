68412be24ce25cf8e6f410ae89de92fa24d3a291
xavierfebrer <xavierfebrer@gmail.com>
2014-12-05 17:15:35 +0100

    Added the red cover when low HP

--- Project/unity/Assets/Menu/Script/HUD.cs
+++ Project/unity/Assets/Menu/Script/HUD.cs
@@ -19,8 +19,9 @@ public class HUD : MonoBehaviour
 	private float mana, alturaMana, manapercent;
 	public Texture[] magicTextures = new Texture[3];
 	public Texture[] magicTexturesSpelled = new Texture[3];
-
+	
 	public Texture texEscut;
+	public Texture texRedCover;
 	//textura que saldra cuando el personaje muera
 	public Texture gameOverTexture,hud_bg;
 	private float xPos;
@@ -128,6 +129,13 @@ public class HUD : MonoBehaviour
 		float alturaMagia = Screen.height / 15;
 		float yMagies = Screen.height - alturaMagia -Screen.height*0.01f;
 
+		if(pj.isAlive() && (((float)vida)/((float)maxVida)) <= 0.5f){
+			Color tmpColor = GUI.color;
+			GUI.color = new Color(1,1,1,1f-((float)vida)/((float)maxVida*0.5f));
+			print("((float)vida)/((float)maxVida) "+((float)vida)/((float)maxVida));
+			GUI.DrawTexture (new Rect (0,0,Screen.width,Screen.height),this.texRedCover);
+			GUI.color = tmpColor;
+		}
 
 		int numTextures = 3;
 
