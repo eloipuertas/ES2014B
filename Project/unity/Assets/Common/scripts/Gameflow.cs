ef47882c6a8bcbf4b1ac75739aa4a468f789ceac
JordiRQ <jrodriqueral92@gmail.com>
2014-12-06 17:55:20 +0100

    Prepared gameflow for the troll and added treatment after it's dead

--- Project/unity/Assets/Common/scripts/Gameflow.cs
+++ Project/unity/Assets/Common/scripts/Gameflow.cs
@@ -3,6 +3,8 @@ using System.Collections;
 
 public class Gameflow : MonoBehaviour {
 	private Transform[] spawnPoints;
+	public Transform trigger_door;
+	public Transform trollSpawner;
 	public float minTimeBetweenSpawns = 10;
 	public float maxTimeBetweenSpawns = 20;
 	public int maxEnemies = 0;
@@ -11,12 +13,12 @@ public class Gameflow : MonoBehaviour {
 	private int phase;
 	private const int INITIAL_PHASE = 1;
 	private const int TROLL_FIGHT = 2;
-	private const int LEVEL_CLEARED = 3;
+	private const int GAME_COMPLETE = 3;
 	private const int GAME_OVER = 0;
 
 	void Awake() {
 		phase = INITIAL_PHASE;
-		
+		if (trigger_door!=null) triggerDoor.GetComponent<triggerDoor>().enabled = false;
 		//Debug.Log("PlayerPrefs.GetString(\"player\"): " + PlayerPrefs.GetString("player"));
 
 		string playerTemplate = PlayerPrefs.GetString("player");
@@ -73,32 +75,55 @@ public class Gameflow : MonoBehaviour {
 					}
 
 					Invoke ("spawnSpiders",2f);
-					//TODO enable door
+					if (trigger_door!=null) triggerDoor.GetComponent<triggerDoor>().enabled = true;
 					//TODO spawn troll
+					/**
+					if (trollSpawner!=null){
+						Object prefab = Resources.LoadAssetAtPath("Assets/Troll/Prefab/troll.prefab", typeof(GameObject));
+						GameObject clone = Instantiate(prefab, Vector3.zero, Quaternion.identity) as GameObject;
+						clone.transform.position = trollSpawner.position;
+						clone.GetComponent<NavMeshAgent> ().enabled = true;
+						clone.GetComponent<TrollState> ().enabled = true;
+					clone.GetComponent<TrollAI> ().enabled = true;
+					}
+					**/
 				}
 			} else if (phase == TROLL_FIGHT) {
 				SpiderState spiderstate;
-				//TrollState trollstate;
-				for (int i=0;i<enemies.Length;i++){
+				TrollState trollstate;
+				for (int i=0;i<enemies.Length && phase==TROLL_FIGHT;i++){
 					enemy = enemies[i];
 					enemyEntity = enemy.GetComponent<AbstractEntity>();
 					if (enemyEntity!=null){
 						if (!enemyEntity.isAlive ()){
 							spiderstate = enemy.GetComponent<SpiderState>();
-							//trollstate = enemy.GetComponent<TrollState>();
+							trollstate = enemy.GetComponent<TrollState>();
 							if (spiderstate!=null){
 								spiderstate.destroyWithDelay(2f);
 								Invoke ("spawnSpiders",2f);
-							}//else if trollstate!=null
-
+							}else if (trollstate!=null){
+								if (!trollstate.isAlive()){
+									phase = GAME_COMPLETE;
+								}
+							}
 						}
 					}
 				}
-			}else if (phase != GAME_OVER){
-				phase = GAME_OVER;
+			}else{
+				for (int i=0;i<enemies.Length;i++){
+					enemy = enemies[i];
+					SpiderState spiderstate = enemy.GetComponent<SpiderState>();
+					if (spiderstate!=null){
+						spiderstate.substractHealth(spiderstate.getHP());
+						spiderstate.destroyWithDelay(1f);
+					}
+				}
 			}
+		}else if (phase!=GAME_OVER){
+			phase = GAME_OVER;
 		}
 	}
+
 	private void spawnSpiders(){
 		//Debug.Log ("Gameflow: spawnSpiders");
 		int curr_enemies = GameObject.FindGameObjectsWithTag ("Enemy").Length;
