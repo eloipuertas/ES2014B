244d5c0770ad7300cfe633aeaca656b22fb0bd59
voriol <vkoriol@gmail.com>
2014-12-07 19:22:04 +0100

    #237 so you win

--- Project/unity/Assets/Terrain/Script/AmbientalMusic.cs
+++ Project/unity/Assets/Terrain/Script/AmbientalMusic.cs
@@ -10,6 +10,9 @@ public class AmbientalMusic : MonoBehaviour
 
 	public AudioClip gameOver;
 
+	public AudioClip youWin;
+	public AudioClip credits;
+
 	public bool pause = false;
 
 	// Use this for initialization
@@ -46,6 +49,16 @@ public class AmbientalMusic : MonoBehaviour
 	public void PlayCatacumba()
 	{
 		audio.clip = catacumba;
+	}
+
+	public void PlayYouWin()
+	{
+		audio.clip = youWin;
+	}
+	
+	public void PlayCredits()
+	{
+		audio.clip = credits;
 	}
 
 	public void PauseAudio()
